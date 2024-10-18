b7956abeb54b47cab0903e5850ed5b1bb856e0d0
1alexandrer <alexandre.ribeiro1509@Gmail.com>
2024-10-18 04:28:43 -0400

    hamburger fix

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -8,19 +8,11 @@ const HamburgerMenu = () => {
     setIsOpen(!isOpen);
   };
 
-
-  const dropdownRef = useRef(null);
-
-  
-
   return (
     <div
       className="cursor-pointer flex bottom-0 left-0 right-0 box-border h-[19px] w-[19px] translate-y-[2px] sm:hidden group"
       onClick={toggleMenu}
     >
-    
-      
-    
       <div className="space-y-2 flex translate-y-1">
         <span
           className={`block absolute h-0.5 w-[19px] origin-center rounded-full  transition-transform ease-in-out duration-100 bg-gray group-hover:bg-[#a6a6a6]
